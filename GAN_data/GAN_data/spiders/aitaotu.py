1f1d7381194686cf84c6cdf9cc462561d26acc51
“xiang2002 <2014176995@qq.com>
2020-09-14 12:57:31 +0800

    origin

---
+++ GAN_data/GAN_data/spiders/aitaotu.py
@@ -0,0 +1,30 @@
+import uuid
+
+import scrapy
+
+from GAN_data.items import GanDataItem
+
+
+class AitaotuSpider(scrapy.Spider):
+    name = 'aitaotu'
+    # allowed_domains = ['https://www.aitaotu.com/tag/tmnh.html']
+    start_urls = ['https://www.aitaotu.com/tag/tmnh.html']
+
+    def parse(self, response):
+        for src in response.xpath("//div[@class='libox']/a/@href").extract():
+            yield scrapy.Request(response.urljoin(src), callback=self.parse_img_link)
+        if response.xpath("//a[text()='下一页']/@href").extract():
+            next_page = response.xpath("//a[text()='下一页']/@href").get()
+            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+
+    def parse_img_link(self, response):
+        item = GanDataItem()
+        img_link = response.xpath("//p/a/img/@src").get()
+        item['name'] = str(uuid.uuid4()).replace("-", "") + '.jpg'
+        item['src'] = img_link
+        yield item
+        img_id = response.url.split("/")[-1].split(".")[0].split("_")[0]
+        if img_id in response.xpath("//li[@class='tal']/p/a/@href").get():
+            next_img = response.xpath("//li[@class='tal']/p/a/@href").get()
+            yield scrapy.Request(response.urljoin(next_img), callback=self.parse_img_link)
+
--- GAN_data/GAN_data/spiders/tpzj.py
+++ GAN_data/GAN_data/spiders/tpzj.py
@@ -12,19 +12,18 @@ class TpzjSpider(scrapy.Spider):
 
     def parse(self, response):
         for src in response.xpath("//div[@class='IndexList']/ul[@class='IndexListult']/li/a/@href").extract():
-            yield scrapy.Request(src, callback=self.parse_img_link)
+            yield scrapy.Request(response.urljoin(src), callback=self.parse_img_link)
         if response.xpath("//div[@id='pageNum']/span/li/a[text()='下一页']/@href").extract():
             next_page = response.xpath("//div[@id='pageNum']/span/li/a[text()='下一页']/@href").get()
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
     def parse_img_link(self, response):
-        if response.url.split("_")[0] != response.url:
-            item = GanDataItem()
-            img_link = response.xpath("//div[@class='show-simg']/a/img/@src").get()
-            item['name'] = str(uuid.uuid4()).replace("-", "")+'.jpg'
-            item['src'] = img_link
-            yield item
-        img_id = response.url.split("/")[-1].split(".")[0].split("_")[0]
+        item = GanDataItem()
+        img_link = response.xpath("//div[@class='show-simg']/a/img/@src").get()
+        img_id = response.url.split("/")[-1].split(".")[0]
+        item['name'] = str(uuid.uuid4()).replace("-", "")+'.jpg'
+        item['src'] = img_link
+        yield item
         if img_id in response.xpath("//div[@class='show-simg']/a/@href").get():
             next_img = response.xpath("//div[@class='show-simg']/a/@href").get()
             yield scrapy.Request(response.urljoin(next_img), callback=self.parse_img_link)
